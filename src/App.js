ffac4eab124547f681c39d67fcd0dae642cdaf4a
dariancmagee <dariancmagee@gmail.com>
2022-05-14 13:51:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [exerciseData, setExerciseData] = useState(null);
+  const [days, setDays] = useState(1);
+
+  function handleChange(e) {
+    setExerciseData(e.target.value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Fitness App</h1>
+        <section className="controls">
+        <input
+        type="text"
+        placeholder="Body part (e.g. legs)"
+        onChange={handleChange} />
+        <input
+        type="number"
+        placeholder="Number of exercises"/>
+        </section>
+        <button>Search</button>
     </div>
   );
 }
